a59f327f6c4bae180b34f9d2cafd19f5aefd00c8
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-02-27 15:50:35 +0630

    update

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,10 @@
 import checkPassportOpenTime from "./handlers/check-passport-open-time";
-import moment from "moment";
+// import moment from "moment";
 
 export const afterMasterProcessStart = async () => {
-  setInterval(() => {
-    console.log(moment().format("hh:mm:ss"));
-  }, 1000);
+  //   setInterval(() => {
+  //     console.log(moment().format("hh:mm:ss"));
+  //   }, 1000);
   const passportInterval = setInterval(() => {
     checkPassportOpenTime(passportInterval);
   }, parseInt(process.env.passport_interval || "0") || 60 * 1000);
